671d0896e8c8b9ae562a04b7df7dafa0eae5a386
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-07-07 10:37:59 +0200

    Add splash activity

---
+++ app/src/main/kotlin/app/splash/SplashActivity.kt
@@ -0,0 +1,48 @@
+package app.splash
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Handler
+
+/**
+ * A simple activity that acts as a splash screen.
+ *
+ * Note how, instead of using the content view to set the splash, we just set the splash as
+ * background in the theme. This allows it to be shown without having to wait for the content view
+ * to be drawn.
+ */
+class SplashActivity : Activity() {
+    private lateinit var handler: Handler
+
+    override fun onResume() {
+        super.onResume()
+        scheduleContentOpening()
+    }
+
+    /**
+     * Schedules the app content to be shown.
+     */
+    private fun scheduleContentOpening() {
+        handler = Handler()
+        handler.postDelayed({ openContent() }, SHOW_TIME_MILLIS)
+    }
+
+    /**
+     * Closes the splash and introduces the actual content of the app.
+     */
+    private fun openContent() {
+//      TODO  val intent = create intent to either the loginactivity or the logged-in activity
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finishAfterTransition()
+    }
+
+    override fun onPause() {
+        handler.removeCallbacksAndMessages(null)
+        super.onPause()
+    }
+
+    private companion object {
+        const val SHOW_TIME_MILLIS = 1000L
+    }
+}
