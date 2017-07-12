66b36a72a18a73a69d2861f80ca2db277c01332e
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-07-12 17:59:10 +0200

    Decouple FacebookLogin into a different feature class

---
+++ app/src/main/kotlin/app/login/FacebookLoginFeature.kt
@@ -0,0 +1,58 @@
+package app.login
+
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginBehavior
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.crash.FirebaseCrash
+import org.stoyicker.dinger.R
+
+internal class FacebookLoginFeature(context: Context, loginButton: LoginButton) {
+    private val callbackManager = CallbackManager.Factory.create()
+
+    init {
+        loginButton.apply {
+            loginBehavior = LoginBehavior.WEB_ONLY
+            registerCallback(callbackManager,
+                    object : FacebookCallback<LoginResult> {
+                        override fun onCancel() {
+                            Toast.makeText(context, R.string.login_cancelled,
+                                    Toast.LENGTH_LONG)
+                                    .show()
+                        }
+
+                        override fun onError(exception: FacebookException) {
+                            FirebaseCrash.report(exception)
+                            Toast.makeText(context, R.string.login_failed,
+                                    Toast.LENGTH_LONG)
+                                    .show()
+                        }
+
+                        override fun onSuccess(loginResult: LoginResult) {
+                            val token = AccessToken.getCurrentAccessToken()
+                            Toast.makeText(context,
+                                    "Token: ${token.token}\nId: ${token.userId}",
+                                    Toast.LENGTH_LONG)
+                                    .show()
+                        }
+                    })
+        }
+    }
+
+    /**
+     * Call from Activity#onActivityResult(Int, Int, Intent?).
+     */
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun release(loginButton: LoginButton) {
+        loginButton.unregisterCallback(callbackManager)
+    }
+}
--- app/src/main/kotlin/app/login/LoginActivity.kt
+++ app/src/main/kotlin/app/login/LoginActivity.kt
@@ -4,65 +4,32 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import com.facebook.AccessToken.getCurrentAccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginBehavior
-import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
-import com.google.firebase.crash.FirebaseCrash
 import kotlinx.android.synthetic.main.activity_login.*
 import org.stoyicker.dinger.R
 
 
 internal class LoginActivity : Activity() {
-    private lateinit var callbackManager: CallbackManager
+    private lateinit var facebookLoginFeature: FacebookLoginFeature
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        bindFacebookLoginCallback(login_button)
+        bindFacebookLoginFeature(login_button)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        callbackManager.onActivityResult(requestCode, resultCode, data)
+        facebookLoginFeature.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun bindFacebookLoginCallback(loginButton: LoginButton) {
-        callbackManager = CallbackManager.Factory.create()
-        loginButton.loginBehavior = LoginBehavior.WEB_ONLY
-        loginButton.registerCallback(callbackManager,
-                object : FacebookCallback<LoginResult> {
-                    override fun onCancel() {
-                        this@LoginActivity.runOnUiThread {
-                            Toast.makeText(this@LoginActivity, R.string.login_cancelled,
-                                    Toast.LENGTH_LONG)
-                                    .show()
-                        }
-                    }
+    override fun onDestroy() {
+        facebookLoginFeature.release(login_button)
+        super.onDestroy()
+    }
 
-                    override fun onError(exception: FacebookException) {
-                        FirebaseCrash.report(exception)
-                        this@LoginActivity.runOnUiThread {
-                            Toast.makeText(this@LoginActivity, R.string.login_failed,
-                                    Toast.LENGTH_LONG)
-                                    .show()
-                        }
-                    }
-
-                    override fun onSuccess(loginResult: LoginResult) {
-                        this@LoginActivity.runOnUiThread {
-                            val token = getCurrentAccessToken()
-                            Toast.makeText(this@LoginActivity,
-                                    "Token: ${token.token}\nId: ${token.userId}",
-                                    Toast.LENGTH_LONG)
-                                    .show()
-                        }
-                    }
-                })
+    private fun bindFacebookLoginFeature(loginButton: LoginButton) {
+        facebookLoginFeature = FacebookLoginFeature(this, loginButton)
     }
 
     companion object {
