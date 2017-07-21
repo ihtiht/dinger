0fe0ee32452635d3e079089770e2fd86eb0a8b08
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-07-21 19:33:57 +0200

    Initialization content provider that allows to remove :data as a dependency from app

---
+++ data/src/main/kotlin/data/InitializationContentProvider.kt
@@ -0,0 +1,27 @@
+package data
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.net.Uri
+import data.auth.InAppAccountManager
+
+/**
+ * Used to tie to the app lifecycle, for things like obtaining a Context reference or initializing
+ * DI.
+ * @see <a href="https://firebase.googleblog.com/2016/12/how-does-firebase-initialize-on-android.html">
+ *     The Firebase Blog: How does Firebase initialize on Android</a>
+ */
+internal class InitializationContentProvider : ContentProvider() {
+    override fun onCreate(): Boolean {
+        InAppAccountManager.context = context
+        return true
+    }
+
+    override fun insert(uri: Uri?, values: ContentValues?) = null
+    override fun query(uri: Uri?, projection: Array<out String>?, selection: String?,
+                       selectionArgs: Array<out String>?, sortOrder: String?) = null
+    override fun update(uri: Uri?, values: ContentValues?, selection: String?,
+                        selectionArgs: Array<out String>?) = 0
+    override fun delete(uri: Uri?, selection: String?, selectionArgs: Array<out String>?) = 0
+    override fun getType(uri: Uri?) = null
+}
