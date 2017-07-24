4d14176f245912e4e59671c6f0c1f50e1c3cbc37
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-07-24 17:32:15 +0200

    During network requests, report exceptions that are not UnknownHostException

--- data/src/main/kotlin/data/network/common/RequestSource.kt
+++ data/src/main/kotlin/data/network/common/RequestSource.kt
@@ -1,14 +1,22 @@
 package data.network.common
 
+import com.google.firebase.crash.FirebaseCrash
 import com.nytimes.android.external.store3.base.impl.Store
 import io.reactivex.Single
+import java.net.UnknownHostException
 
 internal abstract class RequestSource<in RequestModel, ResponseModel>(
         private val store: Store<ResponseModel, RequestModel>)
     : Gettable<RequestModel, ResponseModel> {
     override fun fetch(parameters: RequestModel): Single<ResponseModel> {
         return operate(store.fetch(parameters).onErrorResumeNext { error ->
-            get(parameters).onErrorResumeNext(Single.error(error))
+            get(parameters)
+                    .doOnSuccess { _ ->
+                        if (error !is UnknownHostException) {
+                            FirebaseCrash.report(error)
+                        }
+                    }.
+                    onErrorResumeNext(Single.error(error))
         })
     }
 
