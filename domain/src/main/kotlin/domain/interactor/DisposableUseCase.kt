9f250ac531e83a09dcb0f36978f9d0d98e3a18ea
Jorge Antonio Díaz-Benito Soriano <stoyicker@users.noreply.github.com>
2017-08-30 14:00:42 +0200

    internal -> protected in the disaposable delegate

--- domain/src/main/kotlin/domain/interactor/DisposableUseCase.kt
+++ domain/src/main/kotlin/domain/interactor/DisposableUseCase.kt
@@ -3,7 +3,8 @@ package domain.interactor
 import io.reactivex.disposables.Disposable
 
 abstract class DisposableUseCase internal constructor() {
-    internal var assembledSubscriber: Disposable? = null
+    protected var assembledSubscriber: Disposable? = null
+    
     /**
      * Tears down the use case if required.
      */
