638884483263f1cad0462633d744277bd3b7fe04
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-09-20 13:12:14 +0200

    Fix a bug with disposable subscriber

--- domain/src/main/kotlin/domain/interactor/DisposableUseCase.kt
+++ domain/src/main/kotlin/domain/interactor/DisposableUseCase.kt
@@ -1,16 +1,17 @@
 package domain.interactor
 
 import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
 
 abstract class DisposableUseCase protected constructor() {
-    protected var assembledSubscriber: Disposable? = null
-    
+    protected var assembledSubscriber: Disposable = Disposables.empty()
+
     /**
      * Tears down the use case if required.
      */
-      fun dispose() {
-          if (assembledSubscriber?.isDisposed == false) {
-            assembledSubscriber!!.dispose()
-          }
-      }
+    fun dispose() {
+        if (!assembledSubscriber.isDisposed) {
+            assembledSubscriber.dispose()
+        }
+    }
 }
