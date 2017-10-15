5111314d80a78540d10c8eb532f578518546a5b1
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-10-15 18:32:07 +0200

    Track account name from apps onCreate()

--- app/src/main/kotlin/app/MainApplication.kt
+++ app/src/main/kotlin/app/MainApplication.kt
@@ -1,12 +1,31 @@
 package app
 
 import android.app.Application
+import android.content.Context
 import app.di.ApplicationComponent
 import app.di.DaggerApplicationComponent
+import app.splash.SplashEventTracker
+import app.splash.UserEmailPropertySetterCoordinator
+import org.stoyicker.dinger.R
+import tracker.EventTrackers
 
 /**
  * Custom application.
  */
 internal open class MainApplication : Application() {
     val applicationComponent: ApplicationComponent by lazy { DaggerApplicationComponent.create() }
+
+    override fun onCreate() {
+        super.onCreate()
+        trackAccountName()
+    }
+
+    private fun trackAccountName() =
+            getSharedPreferences(getString(R.string.preferences_file_core), Context.MODE_PRIVATE)
+                    .getString(
+                            UserEmailPropertySetterCoordinator.PREFERENCE_KEY_USER_PROVIDED_ACCOUNT,
+                            null)?.let {
+                SplashEventTracker(this, EventTrackers.firebase())
+                        .setUserProvidedAccount(it)
+            }
 }
