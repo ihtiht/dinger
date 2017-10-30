ca8b9b2cf099ac64e87e45a58037203a7a7aef5a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-10-30 12:17:51 +0100

    Split Travis in per-module jobs

--- ci/script.sh
+++ ci/script.sh
@@ -3,6 +3,6 @@ case "$TRAVIS_COMMIT_MESSAGE" in
         echo "Commit message has [noci]. CI skipped (reported successful)".
         ;;
     *)
-        printenv ANDROID_HOME && ./gradlew clean build dokka :app:check :domain:check :data:check --no-daemon --stacktrace
+        $1
         ;;
 esac
