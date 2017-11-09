d773efe998ba8abeb95a349c4dcc5c999f1badbc
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-09 16:46:40 +0100

    Log requests in Firebase

--- data/src/main/kotlin/data/network/NetworkClientModule.kt
+++ data/src/main/kotlin/data/network/NetworkClientModule.kt
@@ -2,17 +2,41 @@ package data.network
 
 import dagger.Module
 import dagger.Provides
+import data.crash.FirebaseCrashReporterModule
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import okio.Buffer
+import reporter.CrashReporter
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
-@Module
+@Module(includes = arrayOf(FirebaseCrashReporterModule::class))
 internal class NetworkClientModule {
     @Provides
     @Singleton
-    fun client(): OkHttpClient.Builder = OkHttpClient.Builder()
+    fun client(crashReporter: CrashReporter): OkHttpClient.Builder = OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addInterceptor { chain ->
+                chain.request().let {
+                    it.newBuilder().build().let { copy ->
+                        crashReporter.report(IllegalMonitorStateException("""
+                            Method: ${copy.method()}
+                            Https: ${copy.isHttps}
+                            Body: ${Buffer().let { buffer ->
+                                copy.body()?.writeTo(buffer)
+                                ?.also { buffer.readUtf8() } }}
+                            Cache-Control: ${copy.cacheControl()}
+                            Headers: ${copy.headers()}
+                            Tag: ${copy.tag()}
+                            Url: ${copy.url()}
+                            """
+                        ))
+                    }
+                    chain.proceed(it)
+                }
+                chain.proceed(chain.request()).also {
+               crashReporter.report()
+            } }
             .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
