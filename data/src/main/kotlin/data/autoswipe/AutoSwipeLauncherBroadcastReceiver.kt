7de193afd24f1154b2cf7c9522c343807bfd6612
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-11 13:11:19 +0100

    Suppress incorrect warning

--- data/src/main/kotlin/data/autoswipe/AutoSwipeLauncherBroadcastReceiver.kt
+++ data/src/main/kotlin/data/autoswipe/AutoSwipeLauncherBroadcastReceiver.kt
@@ -1,10 +1,12 @@
 package data.autoswipe
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
 internal class AutoSwipeLauncherBroadcastReceiver : BroadcastReceiver() {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context?, intent: Intent?) {
         context?.let { AutoSwipeJobIntentService.trigger(it) }
     }
