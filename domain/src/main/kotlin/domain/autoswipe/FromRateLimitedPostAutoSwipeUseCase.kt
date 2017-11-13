982233c78ff6176c4f54f76a7c647f4eef666bb8
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-13 10:07:59 +0100

    Remove safe swipe interval

--- domain/src/main/kotlin/domain/autoswipe/FromRateLimitedPostAutoSwipeUseCase.kt
+++ domain/src/main/kotlin/domain/autoswipe/FromRateLimitedPostAutoSwipeUseCase.kt
@@ -2,13 +2,11 @@ package domain.autoswipe
 
 import android.content.Context
 import io.reactivex.Scheduler
-import org.stoyicker.dinger.domain.R
 
 class FromRateLimitedPostAutoSwipeUseCase(
         context: Context,
         postExecutionScheduler: Scheduler,
         private val notBeforeMillis: Long)
     : PostAutoSwipeUseCase(context, postExecutionScheduler) {
-    override fun provideNotBeforeMillis(context: Context) =
-            notBeforeMillis + context.resources.getInteger(R.integer.sweep_safe_ms).toLong()
+    override fun provideNotBeforeMillis(context: Context) = notBeforeMillis
 }
