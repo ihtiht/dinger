886b7b87a3b137d54e5a41374bfad23ca0511b24
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-14 17:18:33 +0100

    Complete toolbar

--- app/src/main/kotlin/app/home/HomeActivity.kt
+++ app/src/main/kotlin/app/home/HomeActivity.kt
@@ -4,12 +4,20 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import kotlinx.android.synthetic.main.include_toolbar.*
 import org.stoyicker.dinger.R
 
 internal class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_home, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     companion object {
