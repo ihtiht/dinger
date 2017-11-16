a639f119e21f526eac77ac1a95ab913fe6d7ec17
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-16 20:29:00 +0100

    Add a random delay to from_rate_limited schedules

--- domain/src/main/kotlin/domain/autoswipe/FromRateLimitedPostAutoSwipeUseCase.kt
+++ domain/src/main/kotlin/domain/autoswipe/FromRateLimitedPostAutoSwipeUseCase.kt
@@ -2,11 +2,15 @@ package domain.autoswipe
 
 import android.content.Context
 import io.reactivex.Scheduler
+import java.util.Random
+import java.util.concurrent.TimeUnit
 
 class FromRateLimitedPostAutoSwipeUseCase(
         context: Context,
         postExecutionScheduler: Scheduler,
         private val notBeforeMillis: Long)
     : PostAutoSwipeUseCase(context, postExecutionScheduler) {
-    override fun notBeforeMillis(context: Context) = notBeforeMillis
+    override fun notBeforeMillis(context: Context) = notBeforeMillis +
+            TimeUnit.HOURS.convert((Random().nextInt(5 - 1) + 1).toLong(),
+                    TimeUnit.MILLISECONDS)
 }
