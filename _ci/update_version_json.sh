fe16090d42ea71dc70dd347ec3223e2046ee9075
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-16 20:45:18 +0100

    Trust github.com in the container

--- _ci/update_version_json.sh
+++ _ci/update_version_json.sh
@@ -21,6 +21,7 @@ chmod 600 travis_rsa
 eval `ssh-agent -s`
 ssh-add travis_rsa
 
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch
 git checkout gh-pages
