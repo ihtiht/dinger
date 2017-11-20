49c61f87fc32459bbb4a70f4013c181911149bcb
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-20 19:31:04 +0100

    Add elevation and title to the toolbar

--- app/src/main/kotlin/app/settings/SettingsActivity.kt
+++ app/src/main/kotlin/app/settings/SettingsActivity.kt
@@ -2,9 +2,25 @@ package app.settings
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.include_toolbar.*
+import org.stoyicker.dinger.R
 
 internal class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        toolbar.setTitle(R.string.label_settings)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_left)
+    }
+
     companion object {
         fun getCallingIntent(context: Context) = Intent(context, SettingsActivity::class.java)
     }
