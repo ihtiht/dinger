0cc9d252b8c31d558e761252e0401b6a97012847
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-21 11:03:04 +0100

    'About the app' preference

--- app/src/main/kotlin/app/settings/SettingsPreferenceFragmentCompat.kt
+++ app/src/main/kotlin/app/settings/SettingsPreferenceFragmentCompat.kt
@@ -1,7 +1,9 @@
 package app.settings
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.startIntent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
@@ -11,6 +13,7 @@ internal class SettingsPreferenceFragmentCompat : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.prefs_settings, rootKey)
         initializeSharePreference()
+        initializeAboutTheAppPreference()
     }
 
     private fun initializeSharePreference() {
@@ -18,12 +21,22 @@ internal class SettingsPreferenceFragmentCompat : PreferenceFragmentCompat() {
                 .onPreferenceClickListener = Preference.OnPreferenceClickListener {
             val intent = Intent(Intent.ACTION_SEND).apply {
                 type = "text/plain"
+                @SuppressLint("InlinedApi")
                 flags += Intent.FLAG_ACTIVITY_NEW_DOCUMENT
                 putExtra(Intent.EXTRA_TEXT, context?.getString(
                         R.string.dinger_website_download))
             }
             context?.startIntent(Intent.createChooser(
                     intent, getString(R.string.action_share_title)))
+            true
+        }
+    }
+
+    private fun initializeAboutTheAppPreference() {
+        findPreference(context?.getString(R.string.preference_key_about_the_app))
+                .onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            context?.startIntent(Intent(
+                    Intent.ACTION_VIEW, Uri.parse(getString(R.string.dinger_website))))
             true
         }
     }
