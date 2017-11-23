d15b3245db0e5516de01a6633eb3b904d7cc205e
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-11-23 13:31:59 +0100

    Make a couple of builder providers non-singletons to avoid side-effect problems

--- data/src/main/kotlin/data/network/NetworkClientModule.kt
+++ data/src/main/kotlin/data/network/NetworkClientModule.kt
@@ -4,12 +4,10 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
 
 @Module
 internal class NetworkClientModule {
     @Provides
-    @Singleton
     fun client(): OkHttpClient.Builder = OkHttpClient.Builder()
             .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
--- data/src/main/kotlin/data/network/NetworkModule.kt
+++ data/src/main/kotlin/data/network/NetworkModule.kt
@@ -6,12 +6,10 @@ import org.stoyicker.dinger.data.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
-import javax.inject.Singleton
 
 @Module
 internal class NetworkModule {
     @Provides
-    @Singleton
     fun retrofitBuilder(): Retrofit.Builder = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create())
