a4b9a1df46a46d7b14bcbfdc8ccf7df6b34c8dfa
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-12-28 21:27:09 +0100

    Add some color to the view for debugging purposes

--- app/src/main/kotlin/tinder/matches/MatchesFragment.kt
+++ app/src/main/kotlin/tinder/matches/MatchesFragment.kt
@@ -1,8 +1,21 @@
 package tinder.matches
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.FrameLayout
 
 internal class MatchesFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?) =
+            FrameLayout(context).also {
+                it.background = ColorDrawable(Color.RED)
+            }
+
     companion object {
         fun newInstance() = MatchesFragment()
     }
