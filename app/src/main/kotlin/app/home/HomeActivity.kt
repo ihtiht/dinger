bdb3e1e9498e91a4171816140ca7b61bce6c8463
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-12-28 21:51:15 +0100

    Bound viewpager and bottom menu

--- app/src/main/kotlin/app/home/HomeActivity.kt
+++ app/src/main/kotlin/app/home/HomeActivity.kt
@@ -3,11 +3,13 @@ package app.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import app.settings.SettingsActivity
 import kotlinx.android.synthetic.main.include_home_pager.home_pager
+import kotlinx.android.synthetic.main.include_navigation_bar.navigation_bar
 import kotlinx.android.synthetic.main.include_toolbar.toolbar
 import org.stoyicker.dinger.R
 
@@ -30,6 +32,27 @@ internal class HomeActivity : AppCompatActivity() {
 
     private fun setContentPager() {
         home_pager.adapter = HomeFragmentPagerAdapter(supportFragmentManager)
+        home_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) { }
+
+            override fun onPageScrolled(position: Int,
+                                        positionOffset: Float,
+                                        positionOffsetPixels: Int) { }
+
+            override fun onPageSelected(position: Int) {
+                navigation_bar.menu.getItem(position).isChecked = true
+            }
+        })
+        navigation_bar.setOnNavigationItemSelectedListener {
+            home_pager.currentItem = when (it.itemId) {
+                R.id.navigation_item_matches -> 0
+                R.id.navigation_item_seen -> 1
+                R.id.navigation_item_me -> 2
+                else -> throw IllegalStateException(
+                        "Unexpected menu item index selected ${it.itemId}")
+            }
+            true
+        }
     }
 
     companion object {
