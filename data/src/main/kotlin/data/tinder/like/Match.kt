2d92ecca223ec5783c6760bd39f99cd67e49ee40
Jorge Antonio Díaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-07-07 11:58:37 +0200

    Make Match a data class

--- data/src/main/kotlin/data/tinder/like/Match.kt
+++ data/src/main/kotlin/data/tinder/like/Match.kt
@@ -2,5 +2,5 @@ package data.tinder.like
 
 import com.squareup.moshi.Json
 
-// A bunch of other fields come in the model. Add them as needed
-internal class Match private constructor(@field:Json(name = "id") private val id: String)
+// A bunch of other fields come in the model. Add them as needed for tracking purposes
+internal data class Match private constructor(@field:Json(name = "id") private val id: String)
